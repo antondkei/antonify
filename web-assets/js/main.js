7cd1096b52e15b6cde736074f621910f78f3b0d3
antondkei <antdeekei@gmail.com>
2026-07-08 12:23:09 +0700

    update

--- web-assets/js/main.js
+++ web-assets/js/main.js
@@ -535,3 +535,28 @@ document.querySelectorAll('.liquid-glow').forEach(card => {
         state.targetY = rect.height / 2;
     });
 });
+
+//FAQ Accordion
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        faqItems.forEach(faq => {
+
+            if(faq !== item){
+
+                faq.classList.remove("active");
+
+            }
+
+        });
+
+        item.classList.toggle("active");
+
+    });
+
+});
